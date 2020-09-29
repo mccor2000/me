3315cbe61035f6f72e56bd5d89be7faa930cf8e5
mccor2000 <roccm0200@gmail.com>
2020-09-29 16:25:27 +0700

    feat(api-server): initialize basic routes

--- api-server/src/app.route.js
+++ api-server/src/app.route.js
@@ -1,9 +1,29 @@
 import { Router } from 'express';
+import {
+  getAllProjects,
+  getProject,
+  addProject,
+  updateProject,
+  deleteProject,
+  getFile,
+  comment,
+  feedback
+} from './controllers';
 
 const router = Router();
 
-router.use('/', async (_req, res) => {
+router.use('/hello', async (_req, res) => {
   res.send('hello world');
 });
+
+router.route('/projects').get(getAllProjects).post(addProject);
+router
+  .route('/projects/:id')
+  .get(getProject)
+  .put(updateProject)
+  .delete(deleteProject);
+router.route('/projects/:id/feedback').post(feedback);
+router.route('/projects/:id/file/:id').get(getFile);
+router.route('/projects/:id/file/:id/comment').post(comment);
 
 export default router;

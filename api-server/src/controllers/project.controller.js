5737337a3ea9867e43d4dbce145589867837e8d8
mccor2000 <roccm0200@gmail.com>
2020-09-29 16:25:41 +0700

    feat(api-server): add mock controllers

--- api-server/src/controllers/project.controller.js
+++ api-server/src/controllers/project.controller.js
@@ -1,1 +1,31 @@
-export * from './project.controller';
+export const getAllProjects = async (req, res) => {
+  res.json({ route: req.originalUrl });
+};
+
+export const addProject = async (req, res) => {
+  res.json({ route: req.originalUrl });
+};
+
+export const getProject = async (req, res) => {
+  res.json({ route: req.originalUrl });
+};
+
+export const updateProject = async (req, res) => {
+  res.json({ route: req.originalUrl });
+};
+
+export const deleteProject = async (req, res) => {
+  res.json({ route: req.originalUrl });
+};
+
+export const getFile = async (req, res) => {
+  res.json({ route: req.originalUrl });
+};
+
+export const feedback = async (req, res) => {
+  res.json({ route: req.originalUrl });
+};
+
+export const comment = async (req, res) => {
+  res.json({ route: req.originalUrl });
+};

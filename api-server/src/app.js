4ef924b349ea3ef70298e3b05d5f8a3daf11d319
mccor2000 <roccm0200@gmail.com>
2020-10-03 13:39:18 +0700

    feat(api-server): add compression & helmet middlewares

--- api-server/src/app.js
+++ api-server/src/app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import { json, urlencoded } from 'body-parser';
 import cors from 'cors';
+import helmet from 'helmet';
+import compression from 'compression';
 
 import router from './app.route';
 
@@ -9,6 +11,8 @@ const app = express();
 app.use(json());
 app.use(urlencoded({ extended: true }));
 app.use(cors());
+app.use(helmet());
+app.use(compression());
 
 app.use('/', router);
 
